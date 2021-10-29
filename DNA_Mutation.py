f5bd92af4bfd0aa06104252b4664e7a528ddb548
SAMUELVJOSHUA <89632450+SAMUELVJOSHUA@users.noreply.github.com>
2021-10-29 11:58:01 +0530

    Updated DNA Mutation
    Sequence of DNA after Mutation is obtained

--- DNA_Mutation.py
+++ DNA_Mutation.py
@@ -9,3 +9,12 @@ print (seq_list)
 #To be mutated
 
 pos = int(input("Enter the position to be mutated:"))
+
+print ("The nucleotide base that has been changed is:" ,seq_list.pop(pos - 1))
+next = input("Enter the nucleotide base that is set to be replacing the changed one:")
+seq_list.insert ( pos-1,next)
+print ("The sequence of DNA after mutation is:")
+#for i in range (len(seq_list)):
+  #print(seq_list[i])
+print (' '.join(map(str,seq_list)))
+
