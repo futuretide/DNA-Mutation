5863f474fec101360851d17269abec37488bd6d1
Sri harsha_8668 <56351627+futuretide@users.noreply.github.com>
2021-10-29 10:21:49 +0530

    Create DNA_Mutation.py

---
+++ DNA_Mutation.py
@@ -0,0 +1,11 @@
+#Original DNA sequence
+
+dna_seq = input("Enter the sequence:")
+
+seq_list = list(dna_seq)
+
+print (seq_list)
+
+#To be mutated
+
+pos = int(input("Enter the position to be mutated:"))
